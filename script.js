a824bff3c68a9eca9ba53106cb00de2a7eaf5742
CookieMaster2 <danielsamvalla@gmail.com>
2023-12-07 10:30:31 -0600

    Cambios importantes: Nueva API, imagen default

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+var myModal = new bootstrap.Modal(document.getElementById('movieModal'));
+
 document.addEventListener('DOMContentLoaded', function () {
     const busqueda = document.querySelector("#busqueda");
-    busqueda.addEventListener('input', debounce(searchMovies, 500)); // Debounce for better performance
+    busqueda.addEventListener('input', debounce(searchMovies, 500)); // función 'Debounce' para mejor desempeño
 });
 
-function debounce(func, delay) {
+function debounce(func, delay) { //Función para definir un intervalo de tiempo entre búsquedas
     let timeoutId;
     return function () {
         clearTimeout(timeoutId);
@@ -13,39 +15,50 @@ function debounce(func, delay) {
     };
 }
 
-function searchMovies() {
+async function searchMovies() { //Recibe datos de la API mediante la búsqueda; función asíncrona
     const query = busqueda.value.trim();
 
-    if (query === '') {
-        alert('Please enter a search query');
+    if (query === '') { //Si la barra de búsqueda está vacía...
+        console.log("empty query!");
         return;
     }
 
-    fetch(`https://imdb146.p.rapidapi.com/v1/find/?query=${query}`, {
+    const options = { //Método GET de la API para los datos
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'b564a578c9msh60fbef2a59512e9p1b9cffjsn3e5dc1fac960',
-            'X-RapidAPI-Host': 'imdb146.p.rapidapi.com'
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NWYxNWI2NGUzNzkyYjA0ZGJiMDY4MjI2ZDk4MmY2ZCIsInN1YiI6IjY1NzE1NTBlODU4Njc4MDEyYzkyNzk3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Gtk2cDjC-uqdbAVQgmu4tyLADgNVGTku4J-guKD0Seo'
         }
-    })
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data);
-            getMovies(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    };
+
+    try { //Se reciben los datos de la API de forma paralela
+        const [movies, tv] = await Promise.all([
+            fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=es-MX&page=1`, options).then(response => response.json()),
+            fetch(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=true&language=es-MX&page=1`, options).then(response => response.json())
+        ]);
+
+        getMovies(movies) //Agarra los datos de las películas, y los maqueta
+        // console.log(movies)
+
+        getTv(tv) //Agarra los datos de las series, y los maqueta
+        // console.log(tv)
+
+    } catch (error) { //Error
+        console.log(error)
+    }
+
 }
 
-function getMovies(peliculas) {
+
+function getMovies(peliculas) { //Función que recibe los datos de las películas y las maqueta con #template
     const template_peliculas = document.querySelector("#template-container")
     const fragmento = document.createDocumentFragment()
 
-    peliculas.titleResults.results.forEach(pelicula => {
-        const titulo = pelicula.titleNameText;
+    peliculas.results.forEach(pelicula => {
+        const titulo = pelicula.title;
         console.log(titulo);
-        const imagen = pelicula.titlePosterImageModel ? pelicula.titlePosterImageModel.url : 'defaultImageUrl';
+
+        const imagen = pelicula.poster_path ?? "assets/not-found.jpg";
         console.log(imagen);
 
         const template = document.importNode(template_peliculas.content, true);
@@ -54,12 +67,98 @@ function getMovies(peliculas) {
         nombrePelicula.textContent = titulo;
 
         const posterPelicula = template.querySelector(".poster-pelicula img");
-        posterPelicula.setAttribute("src", imagen);
+
+        if (pelicula.poster_path == undefined) {
+            posterPelicula.setAttribute("src", imagen)
+        } else {
+            posterPelicula.setAttribute("src", "https://image.tmdb.org/t/p/original" + imagen);
+        }
+
+        template.addEventListener('click', () => loadDetailsModalMovie(pelicula));
 
         fragmento.appendChild(template);
     });
 
     const contenedor_de_peliculas = document.querySelector(".movies-container");
-    contenedor_de_peliculas.innerHTML = ""; // Clear existing content
+    // contenedor_de_peliculas.innerHTML = "";
     contenedor_de_peliculas.appendChild(fragmento);
+}
+
+function getTv(series) { //Función que recibe los datos de las series y los maqueta con #template
+    const template_peliculas = document.querySelector("#template-container")
+    const fragmento = document.createDocumentFragment()
+
+    series.results.forEach(serie => {
+        const titulo = serie.name;
+        console.log(titulo);
+
+        const imagen = serie.poster_path ?? "assets/not-found.jpg";
+        console.log(imagen);
+
+        const template = document.importNode(template_peliculas.content, true);
+
+        const nombreSerie = template.querySelector("p");
+        nombreSerie.textContent = titulo;
+
+        const posterSerie = template.querySelector(".poster-pelicula img");
+
+        if (serie.poster_path == undefined) {
+            posterSerie.setAttribute("src", imagen)
+        } else {
+            posterSerie.setAttribute("src", "https://image.tmdb.org/t/p/original" + imagen);
+        }
+
+        template.addEventListener('click', () => loadDetailsModalTV(serie));
+
+        fragmento.appendChild(template);
+    });
+
+    const contenedor_de_peliculas = document.querySelector(".movies-container");
+    // contenedor_de_peliculas.innerHTML = "";
+    contenedor_de_peliculas.appendChild(fragmento);
+}
+
+function clearScreen() { //Limpiar la pantalla cuando se presione el botón principal
+    const contenedor_de_peliculas = document.querySelector(".movies-container");
+    contenedor_de_peliculas.innerHTML = "";
+}
+
+function loadDetailsModalMovie(movie) {
+    const modal = document.getElementById('movieModal');
+    const modalTitle = modal.querySelector('.modal-title');
+    const modalBody = modal.querySelector('.modal-body');
+
+    // Set modal title
+    modalTitle.textContent = movie.title;
+
+    // Clear previous content
+    modalBody.innerHTML = '';
+
+    // Add other details as needed
+    const overview = document.createElement('p');
+    overview.textContent = movie.overview; // Adjust property based on your API response
+    modalBody.appendChild(overview);
+
+    // Show the modal
+    myModal.show();
+}
+
+function loadDetailsModalTV(tv) {
+    const modal = document.getElementById('movieModal');
+    const modalTitle = modal.querySelector('.modal-title');
+    const modalBody = modal.querySelector('.modal-body');
+
+    // Set modal title
+    modalTitle.textContent = tv.name;
+
+    // Clear previous content
+    modalBody.innerHTML = '';
+
+    // Add other details as needed
+    const overview = document.createElement('p');
+    overview.textContent = tv.overview; // Adjust property based on your API response
+    modalBody.appendChild(overview);
+
+    // Show the modal
+    myModal.show();
 }
